8731e59ec7a824997d48914a0452637a55a01994
Roman <roman.zuchowski@gmail.com>
2023-08-29 18:04:55 +0200

    this will place the map

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,10 @@
+// Components imports
+import Map from '@/components/map'
+
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center p-24'>
-      <h1 className='text-xl font-bold'>Open Charge Map</h1>
-      <h2 className='text-base'>Finde deine nächste Ladestation</h2>
-
+    <main className='flex min-h-screen flex-col items-center'>
+      <Map />
     </main>
   )
 }
