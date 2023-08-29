e46c0e03383aab190e0c4a89b246f8864d0bb8a2
Roman <roman.zuchowski@gmail.com>
2023-08-29 20:37:56 +0200

    this prepares for fetching locations

--- client/src/components/map/index.tsx
+++ client/src/components/map/index.tsx
@@ -1,22 +1,31 @@
 'use client'
 
 import mapboxgl from 'mapbox-gl';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function Map() {
+export default function MapContainer() {
     const accessToken = process.env.MAPBOX_KEY as string;
+    const [map, setMap] = useState<mapboxgl.Map | null>(null);
+    
     useEffect(() => {
         mapboxgl.accessToken = accessToken;
-        const map = new mapboxgl.Map({
+        const newMap = new mapboxgl.Map({
             container: 'map', // container ID
             style: 'mapbox://styles/mapbox/streets-v12', // style URL
             center: [13.404954, 52.520008], // starting position [lng, lat]
-            zoom: 9, // starting zoom
+            zoom: 15, // starting zoom
         });
+        setMap(newMap);
         return() => {
-            map.remove()
+            if (map) {
+                map.remove()
+            }
         }
     }, []);
+
+    useEffect(() => {
+       // fetch locations and place markers on the map
+    });
 
     return (
         <div>
