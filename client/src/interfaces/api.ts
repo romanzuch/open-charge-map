ef8720695e4987b5fc9692dad8434e5156fe26cc
Roman <roman.zuchowski@gmail.com>
2023-08-29 16:59:26 +0200

    removed existing client

--- client/src/interfaces/api.ts
+++
@@ -1,7 +0,0 @@
-import Location from './location';
-
-interface APIResponse {
-    data: Location[]; // Define LocationData type to match your API response structure
-};
-
-export default APIResponse;
