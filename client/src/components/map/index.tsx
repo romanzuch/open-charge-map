318de3bd7b02bc2c94d5b252bf388ead26b2c707
Roman <roman.zuchowski@gmail.com>
2023-08-31 10:20:50 +0200

    this will add the data, clustering and markers

--- client/src/components/map/index.tsx
+++ client/src/components/map/index.tsx
@@ -2,10 +2,14 @@
 
 import mapboxgl from 'mapbox-gl';
 import React, { useEffect, useState } from 'react';
+import ApiService from '@/services/apiService';
+import Location from '@/src/models/location';
 
 export default function MapContainer() {
     const accessToken = process.env.MAPBOX_KEY as string;
     const [map, setMap] = useState<mapboxgl.Map | null>(null);
+    const [ markers, setMarkers ] = useState<mapboxgl.Marker | null>(null);
+    const apiService = new ApiService();
     
     useEffect(() => {
         mapboxgl.accessToken = accessToken;
@@ -25,6 +29,78 @@ export default function MapContainer() {
 
     useEffect(() => {
        // fetch locations and place markers on the map
+        const features = apiService.fetchLocations();
+        console.log(features);
+        if (map) {
+            map.addSource('locations', {
+                type: 'geojson',
+                data: features,
+                cluster: false,
+                clusterMaxZoom: 14,
+                clusterRadius: 50
+            });
+            map.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png', (error, image) => {
+                if (error) throw error;
+                map.addImage('custom-marker', image);
+                map.addLayer({
+                    id: 'clusters',
+                    type: 'circle',
+                    source: 'locations',
+                    filter: ['has', 'point_count'],
+                    paint: {
+                        // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
+                        // with three steps to implement three types of circles:
+                        //   * Blue, 20px circles when point count is less than 100
+                        //   * Yellow, 30px circles when point count is between 100 and 750
+                        //   * Pink, 40px circles when point count is greater than or equal to 750
+                        'circle-color': [
+                            'step',
+                            ['get', 'point_count'],
+                            '#51bbd6',
+                            100,
+                            '#f1f075',
+                            750,
+                            '#f28cb1'
+                            ],
+                            'circle-radius': [
+                            'step',
+                            ['get', 'point_count'],
+                            20,
+                            100,
+                            30,
+                            750,
+                            40
+                        ]
+                    }
+                });
+                map.addLayer({
+                    id: 'cluster-count',
+                    type: 'symbol',
+                    source: 'locations',
+                    filter: ['has', 'point_count'],
+                    layout: {
+                    'text-field': ['get', 'point_count_abbreviated'],
+                    'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+                    'text-size': 12
+                    }
+                });
+                map.addLayer({
+                    'id': 'points',
+                    'type': 'symbol',
+                    'source': 'locations',
+                    'layout': {
+                    'icon-image': 'custom-marker',
+                    'text-field': ['get', 'properties.address.title'],
+                    'text-font': [
+                        'Open Sans Semibold',
+                        'Arial Unicode MS Bold'
+                    ],
+                    'text-offset': [0, 1.25],
+                    'text-anchor': 'top'
+                    }
+                });
+            });
+        }
     });
 
     return (
